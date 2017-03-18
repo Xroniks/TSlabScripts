0464b861f8edcb5b98bbc4a0880b5725c25012de
Postnikov Maksim <postnikovmaksim@gmail.com>
2017-03-18 15:28:03 +0300

    fix

--- TSlabScripts/Simple.cs
+++ TSlabScripts/Simple.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/**
+ * Описание:
+ * Скрипт для работы на интервале 5 секунд, данные будут сжаты до 5 минут.
+ * На иных таймфреймах работать не будет.
+ * 
+ * Параметры для оптимизации:
+ *  - HistorySource - выставить "1" если используются исторические данные
+ */
+
+using System;
 using System.Linq;
 using System.Collections.Generic;
 using TSLab.DataSource;
@@ -10,44 +19,69 @@ namespace TSLabScripts
 {
     public class Simple : IExternalScript
     {
-        //public OptimProperty HistoryStartBar = new OptimProperty(1,0,);
+        public OptimProperty HistorySource = new OptimProperty(0,0,1,1);
 
         public virtual void Execute(IContext ctx, ISecurity source)
         {
-            // Генерация графика
-            var mainPain = ctx.CreatePane("Simple", 20, false);
-            var mainChart = mainPain.AddList(source.Symbol, source, CandleStyles.BAR_CANDLE, new Color(100, 100, 100),
+            // Проверяем таймфрейм входных данных
+            if (!GetValidTimeFrame(ctx, source)) return;
+
+            // Генерация графика исходного таймфрейма
+            var pain = ctx.CreatePane("Original", 20, false);
+            pain.AddList(source.Symbol, source, CandleStyles.BAR_CANDLE, new Color(100, 100, 100), PaneSides.RIGHT);
+
+            // Компрессия исходного таймфрейма в пятиминутный
+            var compressSource = source.CompressTo(5);
+
+            // Генерация графика с новым таймфреймом
+            var compressPain = ctx.CreatePane("Compress", 20, false);
+            var compressChart = compressPain.AddList(source.Symbol, compressSource, CandleStyles.BAR_CANDLE, new Color(100, 100, 100),
                 PaneSides.RIGHT);
 
-            // Не торгует на интервалах - День и Тик
-            if (source.IntervalBase == DataIntervals.DAYS || source.IntervalBase == DataIntervals.TICK) return;
-
-            //Конвертация значений
-            var highPriceList = source.HighPrices.ToList();
-            var lowPriceList = source.LowPrices.ToList();
-
-            //SearchPosition(source, source.Bars.Count);
-            //SearchBuyModel(source, source.Bars.Count - 1, highPriceList, lowPriceList, mainChart);
-            //SearchSellModel(source, source.Bars.Count - 1, highPriceList, lowPriceList, mainChart);
-
-            for (var historyBar = 1; historyBar <= source.Bars.Count - 1; historyBar++)
+            if (HistorySource.Value == 1)
             {
-                SearchPosition(source, historyBar + 1);
-                SearchBuyModel(source, historyBar, highPriceList, lowPriceList, mainChart);
-                SearchSellModel(source, historyBar, highPriceList, lowPriceList, mainChart);
+                ctx.Log("Исторические данные",new Color(),true);
+                for (var historyBar = 1; historyBar <= source.Bars.Count - 1; historyBar++)
+                {
+                    Trading(ctx, source, compressSource, historyBar, compressChart);
+                }
             }
+            else
+            {
+                Trading(ctx, source, compressSource, source.Bars.Count - 1, compressChart);
+            }   
         }
 
-        public void SearchBuyModel(ISecurity source, int bar, List<double> highPriceList, List<double> lowPriceList, IGraphList mainChart)
+        public void Trading(IContext ctx, ISecurity source, ISecurity compressSource, int bar, IGraphList compressChart)
         {
-            // Не торгует на вечерней сессии
+            // Если время 18:40 или более - закрыть все активные позиции и не торговать
             if (source.Bars[bar].Date.TimeOfDay >= new TimeSpan(18, 40, 00))
             {
-                CloseAllPosition(source, bar + 1);
+                if (source.Positions.ActivePositionCount > 0)
+                {
+                    CloseAllPosition(source, bar);
+                }
                 return;
             }
 
-            var indexBarBeginDay = GetIndexBarBeginDay(source, bar);
+            // Посик активных позиций
+            SearchActivePosition(source, bar);
+
+            var indexCompressBar = GetIndexCompressBar(bar);
+            var indexBeginDayBar = GetIndexBarBeginDay(compressSource, indexCompressBar);
+
+            // Поиск моделей на покупку и выставление для них ордеров
+            SearchBuyModel(compressSource, indexCompressBar, indexBeginDayBar);
+
+            // Поиск моделей на продажу и выставление для них ордеров
+            //SearchSellModel(compressSource, bar, highPriceList, lowPriceList, compressChart);
+
+
+        }
+
+        public void SearchBuyModel(ISecurity compressSource, int indexCompressBar, int indexBeginDayBar)
+        {
+            
             
             for (var indexPointA = bar - 1; indexPointA >= indexBarBeginDay && indexPointA >= 0; indexPointA--)
             {
@@ -87,52 +121,52 @@ namespace TSLabScripts
             }   
         }
 
-        public void SearchSellModel(ISecurity source, int bar, List<double> highPriceList, List<double> lowPriceList, IGraphList mainChart)
-        {
-            // Не торгует на вечерней сессии
-            if (source.Bars[bar].Date.TimeOfDay >= new TimeSpan(18, 40, 00)) return;
+//        public void SearchSellModel(ISecurity source, int bar, List<double> highPriceList, List<double> lowPriceList, IGraphList mainChart)
+//        {
+//            // Не торгует на вечерней сессии
+//            if (source.Bars[bar].Date.TimeOfDay >= new TimeSpan(18, 40, 00)) return;
+//
+//            var indexBarBeginDay = GetIndexBarBeginDay(source, bar);
+//
+//            for (var indexPointA = bar - 1; indexPointA >= indexBarBeginDay && indexPointA >= 0; indexPointA--)
+//            {
+//                // Находим точку B
+//                var rangeFindPointB = lowPriceList.GetRange(indexPointA, bar - indexPointA + 1);
+//                var lowPricePointB = rangeFindPointB.Min();
+//                var indexPointB = indexPointA + rangeFindPointB.IndexOf(lowPricePointB);
+//
+//                // Корректируем точку A
+//                var realRangePointA = highPriceList.GetRange(indexPointA, indexPointB - indexPointA + 1);
+//                var realHighPricePointA = realRangePointA.Max();
+//                var realIncexPointA = indexPointA + realRangePointA.IndexOf(realHighPricePointA);
+//
+//                // Проверм размер фигуры A-B
+//                var ab = realHighPricePointA - lowPricePointB;
+//                if (ab <= 400 || ab >= 1000) continue;
+//
+//                // Находим точку C
+//                var rangeFindPointC = highPriceList.GetRange(indexPointB + 1, bar - indexPointB);
+//                if (rangeFindPointC.Count == 0) continue;
+//                var highPricePointC = rangeFindPointC.Max();
+//                var indexPointC = indexPointB + rangeFindPointC.IndexOf(highPricePointC);
+//
+//                // Проверям размер модели B-C
+//                var bc = highPricePointC - lowPricePointB;
+//                var ac = realHighPricePointA - highPricePointC;
+//                if (bc <= 389 || ac < 0) continue;
+//
+//                mainChart.SetColor(realIncexPointA, new Color(255, 0, 0));
+//                mainChart.SetColor(indexPointB, new Color(0, 255, 0));
+//                mainChart.SetColor(indexPointC, new Color(0, 0, 255));
+//
+//                if (!(highPriceList.GetRange(indexPointC, bar - indexPointC).Min() <= lowPricePointB + 50))
+//                {
+//                    source.Positions.SellIfLess(bar + 1, 1, lowPricePointB +50, "sell_" + lowPricePointB);
+//                }
+//            }
+//        }
 
-            var indexBarBeginDay = GetIndexBarBeginDay(source, bar);
-
-            for (var indexPointA = bar - 1; indexPointA >= indexBarBeginDay && indexPointA >= 0; indexPointA--)
-            {
-                // Находим точку B
-                var rangeFindPointB = lowPriceList.GetRange(indexPointA, bar - indexPointA + 1);
-                var lowPricePointB = rangeFindPointB.Min();
-                var indexPointB = indexPointA + rangeFindPointB.IndexOf(lowPricePointB);
-
-                // Корректируем точку A
-                var realRangePointA = highPriceList.GetRange(indexPointA, indexPointB - indexPointA + 1);
-                var realHighPricePointA = realRangePointA.Max();
-                var realIncexPointA = indexPointA + realRangePointA.IndexOf(realHighPricePointA);
-
-                // Проверм размер фигуры A-B
-                var ab = realHighPricePointA - lowPricePointB;
-                if (ab <= 400 || ab >= 1000) continue;
-
-                // Находим точку C
-                var rangeFindPointC = highPriceList.GetRange(indexPointB + 1, bar - indexPointB);
-                if (rangeFindPointC.Count == 0) continue;
-                var highPricePointC = rangeFindPointC.Max();
-                var indexPointC = indexPointB + rangeFindPointC.IndexOf(highPricePointC);
-
-                // Проверям размер модели B-C
-                var bc = highPricePointC - lowPricePointB;
-                var ac = realHighPricePointA - highPricePointC;
-                if (bc <= 389 || ac < 0) continue;
-
-                mainChart.SetColor(realIncexPointA, new Color(255, 0, 0));
-                mainChart.SetColor(indexPointB, new Color(0, 255, 0));
-                mainChart.SetColor(indexPointC, new Color(0, 0, 255));
-
-                if (!(highPriceList.GetRange(indexPointC, bar - indexPointC).Min() <= lowPricePointB + 50))
-                {
-                    source.Positions.SellIfLess(bar + 1, 1, lowPricePointB +50, "sell_" + lowPricePointB);
-                }
-            }
-        }
-
-        public void SearchPosition(ISecurity source, int bar)
+        public void SearchActivePosition(ISecurity source, int bar)
         {
 
             var positionList = from position in source.Positions
@@ -146,12 +180,12 @@ namespace TSLabScripts
                 switch (arr[0])
                 {
                     case "buy":
-                        position.CloseAtProfit(bar, Convert.ToDouble(arr[1]) + 100, "closeProfit");
-                        position.CloseAtStop(bar, Convert.ToDouble(arr[1]) - 300, "closeStop");
+                        position.CloseAtProfit(bar + 1, Convert.ToDouble(arr[1]) + 100, "closeProfit");
+                        position.CloseAtStop(bar + 1, Convert.ToDouble(arr[1]) - 300, "closeStop");
                         break;
                     case "sell":
-                        position.CloseAtProfit(bar, Convert.ToDouble(arr[1]) - 100, "closeProfit");
-                        position.CloseAtStop(bar, Convert.ToDouble(arr[1]) + 300, "closeStop");
+                        position.CloseAtProfit(bar + 1, Convert.ToDouble(arr[1]) - 100, "closeProfit");
+                        position.CloseAtStop(bar + 1, Convert.ToDouble(arr[1]) + 300, "closeStop");
                         break;
                 }
             }
@@ -169,20 +203,26 @@ namespace TSLabScripts
             }
         }
 
-        public int GetIndexBarBeginDay(ISecurity source, int historyBar)
+        public int GetIndexBarBeginDay(ISecurity compressSource, int indexCompressBar)
         {
-            var timeBeginDay = new TimeSpan(10, 00, 00);
-            var timeСurrentBar = source.Bars[historyBar].Date.TimeOfDay;
-            int countBar;
+            return ((int)compressSource.Bars[indexCompressBar].Date.TimeOfDay.TotalMinutes - 600)/5;
+        }
 
-            if (source.IntervalBase == DataIntervals.MINUTE)
+        public bool GetValidTimeFrame(IContext ctx, ISecurity source)
+        {
+            if (source.IntervalBase == DataIntervals.SECONDS && source.Interval == 5) return true;
+            ctx.Log("Выбран не верный таймфрейм, выберите таймфрейм равный 5 секундам",new Color(255,0,0),true);
+            return false;
+        }
+
+        public int GetIndexCompressBar(int bar)
+        {
+            var tempbar = bar;
+            while (tempbar % 60 != 0)
             {
-                countBar = Convert.ToInt32((timeСurrentBar.TotalMinutes - timeBeginDay.TotalMinutes) / source.Interval);
-                return countBar <= 0 ? 0 : historyBar - countBar;
+                tempbar--;
             }
-
-            countBar = Convert.ToInt32((timeСurrentBar.TotalSeconds - timeBeginDay.TotalSeconds) / source.Interval);
-            return countBar <= 0 ? 0 : historyBar - countBar;
+            return tempbar / 60;
         }
     }
 }
