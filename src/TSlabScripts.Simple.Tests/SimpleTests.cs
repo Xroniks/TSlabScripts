9f1bce91703a579cb12f6a787f8cdb91d684b047
Postnikov Maksim <postnikovmaksim@gmail.com>
2017-07-09 12:44:21 +0300

    тестовые unit test

---
+++ src/TSlabScripts.Simple.Tests/SimpleTests.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using TSLab.Script;
+using TSLab.Script.Handlers;
+
+namespace TSlabScripts.Simple.Tests
+{
+    [TestFixture]
+    public class SimpleTests
+    {
+        private Mock<IContext> contextMock;
+        private Mock<ISecurity> securityMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var fixture = new Fixture();
+            fixture.Customize(new AutoConfiguredMoqCustomization());
+
+            contextMock = new Mock<IContext>();
+            securityMock = new Mock<ISecurity>();
+
+            fixture.Register(() => contextMock);
+            fixture.Register(() => securityMock);
+        }
+
+        [Test]
+        [TestCase]
+        public void GetValidTimeFrame()
+        {
+
+            Simple.GetValidTimeFrame(contextMock, securityMock);
+        }
+    }
+}
